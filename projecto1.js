cfffcad57f3e6772b0ff1e0dca20ebf2fd347ff0
Mariela Rivero <121963413+codemamacode@users.noreply.github.com>
2023-01-25 18:01:42 +0100

    Add files via upload

---
+++ projecto1.js
@@ -0,0 +1,12 @@
+function bulb_on() {
+    document.getElementById("bulb").src =
+      "bulbon.jpg"
+      document.body.style.backgroundColor = "#FCFCFC";
+  }
+  function bulb_off() {
+    document.getElementById("bulb").src =
+      "bulboff.jpg" 
+      document.body.style.backgroundColor = "#070000";
+  }
+
+  
